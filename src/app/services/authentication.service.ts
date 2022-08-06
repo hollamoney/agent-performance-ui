4564783de04d8360890c9e74687bcba8aa778d18
aekartalci <aekartalci@gmail.com>
2022-08-06 19:11:12 +0300

    asfdasd

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -5,7 +5,12 @@ import {User} from "../models/user.model";
 import {HttpClient} from "@angular/common/http";
 import {map} from "rxjs/operators";
 
+if(environment.BASE_URL == null)
+{
+  const API_URL = `/auth/`
+}
 const API_URL = `${environment.BASE_URL}/auth/`
+
 
 @Injectable({
   providedIn: 'root'
